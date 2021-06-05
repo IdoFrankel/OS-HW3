9f848328f6bd5bb90f044d1e325faf863cb45459
Ido Frankel <idofrankel@gmail.com>
2021-06-05 18:29:13 +0000

    Fix dequeueByFd header in queue.h

--- queue.h
+++ queue.h
@@ -4,7 +4,7 @@ struct queue *createQueue(int maxSize);
 
 void enqueue_noLock(struct queue *, int);
 int dequeue_noLock(struct queue *);
-int dequeueByFd(struct queue *, pthread_mutex_t *, pthread_cond_t *, int);
+int dequeueByFd(struct queue *, int);
 
 int size(struct queue *);
 int maxSize(struct queue *);
